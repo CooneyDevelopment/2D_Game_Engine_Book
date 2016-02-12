e997c74f7e0d520133462a556085461b6f34147b
CooneyDevelopment <CooneyDevelopment@gmail.com>
2016-02-11 22:25:41 -0500

    End of Section 3.3

--- Scripts/Engine/Renderable.js
+++ Scripts/Engine/Renderable.js
@@ -3,14 +3,17 @@
 function Renderable(shader){
     this.Shader = shader;
     this.Color = [1.0, 1.0, 1.0, 1.0];
+    this.Transform = new Transform();
 }
 
-Renderable.prototype.Draw = function(modelTransform){
+Renderable.prototype.Draw = function(){
     var _GL = GameEngine.Core.GetGL();
     this.Shader.ActivateShader(this.Color);
-    this.Shader.LoadObjectTransform(modelTransform);
+    this.Shader.LoadObjectTransform(this.Transform.GetTransform());
     _GL.drawArrays(_GL.TRIANGLE_STRIP, 0, 4);
 };
 
 Renderable.prototype.SetColor = function(color){ this.Color = color; };
 Renderable.prototype.GetColor = function(){ return this.Color; };
+
+Renderable.prototype.GetTransform = function(){ return this.Transform; };
--- Scripts/Game/Game.js
+++ Scripts/Game/Game.js
@@ -13,16 +13,14 @@ function Game(canvasId){
     this.RedSquare.SetColor([1.0, 0.0, 0.0, 1.0]);
 
     GameEngine.Core.ClearCanvas([0.0, 0.8, 0.0, 1.0]);
-    var _transform = mat4.create();
 
-    mat4.translate(_transform, _transform, vec3.fromValues(-0.25, 0.25, 0.0));
-    mat4.rotateZ(_transform, _transform, 0.2);
-    mat4.scale(_transform, _transform, vec3.fromValues(1.2, 1.2, 1.0));
-    this.WhiteSquare.Draw(_transform);
+    this.WhiteSquare.GetTransform().SetPosition(-0.25, 0.25);
+    this.WhiteSquare.GetTransform().SetScale(1.2, 1.2);
+    this.WhiteSquare.GetTransform().SetRotationInRadians(0.2);
+    this.WhiteSquare.Draw();
 
-    mat4.identity(_transform);
-    mat4.translate(_transform, _transform, vec3.fromValues(0.25, -0.25, 0.0));
-    mat4.rotateZ(_transform, _transform, -0.8);
-    mat4.scale(_transform, _transform, vec3.fromValues(0.4, 0.4, 1.0));
-    this.RedSquare.Draw(_transform);
+    this.RedSquare.GetTransform().SetPosition(0.25, -0.25);
+    this.RedSquare.GetTransform().SetScale(0.4, 0.4);
+    this.RedSquare.GetTransform().SetRotationInRadians(-0.8);
+    this.RedSquare.Draw();
 }
